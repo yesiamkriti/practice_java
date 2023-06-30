7f51515fbe27abc4116f7295024d4ff87b48b5b7
kriti <kritijulia@gmail.com>
2023-06-30 13:23:56 +0530

    int, float,boolean,char,String

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes {
+    public static void main(String[] args){
+        int distance = 1443123894; // only 10 place digit comes under int
+        long dist = 438509148507342L ;// long interger data type
+        float dis = 23.4797846525f; //float i.e decimal number upto 6 digit
+        boolean di = true;
+        char Du = 'H';
+        String D = "I am here to measure diatance";
+        System.out.println(distance);
+        System.out.println(dist);
+        System.out.println(dis);
+        System.out.println(di);
+        System.out.println(Du);
+        System.out.println(D);
+    }
+}
