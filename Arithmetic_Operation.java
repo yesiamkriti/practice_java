3b5614df0b2ff6a5402cf39e6253d9a91519fea1
kriti <kritijulia@gmail.com>
2024-01-14 16:35:39 +0530

    arithmetic operation

---
+++ Arithmetic_Operation.java
@@ -0,0 +1,23 @@
+public class Arithmetic_Operation {
+    public static void main(String[] args){
+        System.out.println("Calculator");
+        int x = 5;
+        int y = 7;
+        int add = x+y;
+        int sub = x-y;
+        int mul = x*y;
+        int div = x/y;
+        int mod = x%y;
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(mod);
+        System.out.println("maths class");
+        System.out.println(Math.max(5,6));
+        System.out.println(Math.min(5,6));
+        System.out.println(Math.random());
+        System.out.println((int)Math.random()); //0
+        System.out.println((int)(Math.random() * 100));
+    }
+}
