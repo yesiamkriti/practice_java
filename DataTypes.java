7eefd99e831be1b2e9d9a842a91410cae928adf7
kriti <kritijulia@gmail.com>
2024-01-16 21:07:24 +0530

    all datatypes

--- DataTypes.java
+++ DataTypes.java
@@ -3,12 +3,14 @@ public class DataTypes {
         int distance = 1443123894; // only 10 place digit comes under int
         long dist = 438509148507342L ;// long interger data type
         float dis = 23.4797846525f; //float i.e decimal number upto 6 digit
+        double disd = 23.32894775949783475893758937895; // decimal number upto 15 digits
         boolean di = true; // boolen letters should be in lower case 
         char Du = 'H'; //character should be in single quotes
         String D = "I am here to measure diatance";// spelling of  string should be started with capital letter
         System.out.println(distance);
         System.out.println(dist);
         System.out.println(dis);
+        System.out.println(disd);
         System.out.println(di);
         System.out.println(Du);
         System.out.println(D);
